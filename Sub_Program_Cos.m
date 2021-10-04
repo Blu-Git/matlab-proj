56ab1e6b6188df6cf4af732fc26e911e9ed320bb
Kristian Emmanuel Padilla <91727573+5mKP@users.noreply.github.com>
2021-10-04 21:40:17 +0800

    Update Sub_Program_Cos.m

--- Sub_Program_Cos.m
+++ Sub_Program_Cos.m
@@ -13,7 +13,12 @@ TF = islocalmin(y);
 TF1 = islocalmax(y);
 
 %x-intercepts
-[xInt,yInt] = Intersections_TrigGraph(x,y,x,zeros(size(y)));
+if d==0
+    [xInt,yInt] = Intersections_TrigGraph(x,y,x,zeros(size(y)));
+elseif d~=0 & cyc==1
+    x1=EP/4;
+    x2=(EP+(EP/2))/2;
+end
 
 %Plotting
 plot(x,y,'k-');
@@ -28,6 +33,11 @@ grid on
 
 plot(x,y,x(TF),y(TF),'r*');
 plot(x,y,x(TF1),y(TF1),'r*');
-plot(xInt,yInt, 'm*','MarkerSize', 10);
+if d==0
+    plot(xInt,yInt, 'm*','MarkerSize', 10);
+elseif d~=0 & cyc==1
+    plot(x1,d,'m*','MarkerSize', 10);
+    plot(x2,d,'m*','MarkerSize', 10);
+end
 hold off
 end
