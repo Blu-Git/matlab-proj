d5515b9a1fe992b6074110413d9fa9975ed23eb9
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2021-10-04 20:27:29 +0800

    Update Sub_Program_Csc.m

--- Sub_Program_Csc.m
+++ Sub_Program_Csc.m
@@ -1,17 +1,33 @@
-function y=F(a,b,c,d,cyc,P,EP,p,trigFunc)
-syms x
-y=a*csc(b*x+c)+d;
-fplot(y,[0 EP])
+function y=F(a,b,c,d,cyc,P,EP,x,p,trigFunc)
+if b>0
+    y=a*csc(b*x+c)+d;
+    fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx+%d)+%d\n\n', a, trigFunc, b, c, d);
+else
+    b=abs(b);
+    y=-a*csc(b*x+c)+d;
+    fprintf('\nEquation of Trigonometric Function: y=-%d*%s(%dx+%d)+%d\n\n', a, trigFunc, b, c, d);
+end
+
+%Minima and Maxima
+%TF = islocalmin(y);
+%TF1 = islocalmax(y);
+
+%x-intercepts
+[xInt,yInt] = Intersections_TrigGraph(x,y,x,zeros(size(y)));
+
+%Plotting
+plot(x, y,'k-');
 set(gca,'xtick',p)
 set(gca,'xticklabel',string(sym(p)))
 yline(d,'--b');
 xlabel('x')
 ylabel('y')
 title('Graph of Cosecant Function')
+hold on
 grid on
-end
 
-%Add syntax for outputing equation (if-construct)
-%Check syntax
-%Add "Coordinate-labels" c/o Aldy 
-%   (x-intercepts, minima, & maxima of one cycle).
+%plot(x,y,x(TF),y(TF),'r*');
+%plot(x,y,x(TF1),y(TF1),'r*');
+plot(xInt,yInt, 'm*','MarkerSize', 10);
+hold off
+end
