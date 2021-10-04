1b9e60ca1a184619a6f487e52b7bd9d61bc9a20f
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2021-10-04 18:51:31 +0800

    Update Sub_Program_Cos.m

--- Sub_Program_Cos.m
+++ Sub_Program_Cos.m
@@ -1,14 +1,52 @@
-function y=B(a,b,c,d,cyc,P,EP,p,trigFunc)
-syms x
-y=a*cos(b*x+c)+d;
-fplot(y,[0 EP])
+function y=B(a,b,c,d,cyc,P,EP,x,p,trigFunc)
+y = a*cos(b*x+c)+d;
+
+%Minima and Maxima
+TF = islocalmin(y);
+TF1 = islocalmax(y);
+
+%if d==0
+[xInt,yInt] = Intersections_TrigGraph(x,y,x,zeros(size(y)));
+%else
+%    [xInt,yInt];
+%end
+
+%Plotting
+plot(x,y,'k-');
 set(gca,'xtick',p)
 set(gca,'xticklabel',string(sym(p)))
 yline(d,'--b');
 xlabel('x')
 ylabel('y')
 title('Graph of Cosine Function')
+hold on
 grid on
+
+plot(x,y,x(TF),y(TF),'r*');
+plot(x,y,x(TF1),y(TF1),'r*');
+plot(xInt,yInt, 'm*','MarkerSize', 10);
+
+%Character Output
+if c==0
+fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx)+%d\n\n', a, trigFunc, b, d);
+else
+    if d==0
+        fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx+%d)\n\n', a, trigFunc, b, c);
+    elseif c==0 && d==0
+        fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx)\n\n', a, trigFunc, b);
+    elseif c~=0 || d~=0
+        fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx+%d)+%d\n\n', a, trigFunc, b, c, d);
+    elseif a==1
+        fprintf('\nEquation of Trigonometric Function: y=%s(%dx+%d)+%d\n\n', trigFunc, b, c, d);
+    elseif b==1
+        fprintf('\nEquation of Trigonometric Function: y=%d*%s(x+%d)+%d\n\n', a, trigFunc, c, d);
+    elseif c<0
+        fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx%d)+%d\n\n', a, trigFunc, b, c, d);
+    elseif d<0
+        fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx+%d)%d\n\n', a, trigFunc, b, c);
+    end
+end
+
 end
 
 %Add syntax for outputing equation (if-construct)
