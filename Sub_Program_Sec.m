e271a8dc80f0590e3735f741576945bd12272375
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2021-10-05 08:45:42 +0800

    Update Sub_Program_Sec.m

--- Sub_Program_Sec.m
+++ Sub_Program_Sec.m
@@ -1,7 +1,5 @@
 function y=E(a,b,c,d,cyc,P,EP,p,trigFunc)
 syms x
-%y=a*sec(b*x+c)+d;
-%fplot(y,[0 EP])
 if b>0
     y=a*sec(b*x+c)+d;
     fprintf('\nEquation of Trigonometric Function: y=%d*%s(%dx+%d)+%d\n\n', a, trigFunc, b, c, d);
@@ -10,13 +8,6 @@ else
     y=-a*sec(b*x+c)+d;
     fprintf('\nEquation of Trigonometric Function: y=-%d*%s(%dx+%d)+%d\n\n', a, trigFunc, b, c, d);
 end
-
-%Minima and Maxima
-%TF = islocalmin(y);
-%TF1 = islocalmax(y);
-
-%x-intercepts
-%[xInt,yInt] = Intersections_TrigGraph(x,y,x,zeros(size(y)));
 
 %Plotting
 fplot(y,[0 EP],'k-');
@@ -28,9 +19,5 @@ ylabel('y')
 title('Graph of Secant Function')
 hold on
 grid on
-
-%plot(x,y,x(TF),y(TF),'r*');
-%plot(x,y,x(TF1),y(TF1),'r*');
-%plot(xInt,yInt, 'm*','MarkerSize', 10);
 hold off
 end
